e5782045fc079d3edb1b65da51d21c067dfd89f9
ErichClark <erich.clark@gmail.com>
2018-08-26 21:59:52 -0400

    Added code from Udacity to taskForGETMethod and changed return type to Data for GET, POST, Image methods in client

--- TheMovieManager/TMDBClient.swift
+++ TheMovieManager/TMDBClient.swift
@@ -30,22 +30,76 @@ class TMDBClient : NSObject {
     override init() {
         super.init()
     }
-
+    
     // MARK: GET
     
-    func taskForGETMethod(_ method: String, parameters: [String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void)  {
+    func taskForGETMethod(_ method: String, parameters: [String:AnyObject], completionHandlerForGET: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) -> Data {
         
+        var returnData: Data? = nil
+        
+        /* 1. Set the parameters */
+        var parametersWithApiKey = parameters
+        parametersWithApiKey[ParameterKeys.ApiKey] = Constants.ApiKey as AnyObject?
+        
+        /* 2/3. Build the URL, Configure the request */
+        let request = NSMutableURLRequest(url: TMDBClient.tmdbURLFromParameters(parametersWithApiKey, withPathExtension: method))
+        
+        /* 4. Make the request */
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForGET(nil, NSError(domain: "taskForGETMethod", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(String(describing: error))")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            returnData = data
+            
+            /* 5/6. Parse the data and use the data (happens in completion handler) */
+            //            self.convertDataWithCompletionHandler(data, completionHandlerForConvertData: completionHandlerForGET)
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+        return returnData!
         
     }
     
     // MARK: POST
     
-    //func taskForPOSTMethod(_ method: String, parameters: [String:AnyObject], jsonBody: String, completionHandlerForPOST: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) {}
+//    func taskForPOSTMethod(_ method: String, parameters: [String:AnyObject], jsonBody: String, completionHandlerForPOST: @escaping (_ result: AnyObject?, _ error: NSError?) -> Void) {
+//
+//        var returnData: Data? = nil
+    
+//        data = returnData!
+//
+//        task.resume()
+//        return returnData
+//    }
     
     // MARK: GET Image
     
-    func taskForGETImage(_ size: String, filePath: String, completionHandlerForImage: @escaping (_ imageData: Data?, _ error: NSError?) -> Void)  {
+    func taskForGETImage(_ size: String, filePath: String, completionHandlerForImage: @escaping (_ imageData: Data?, _ error: NSError?) -> Void) -> Data {
         
+        var returnData: Data? = nil
         /* 1. Set the parameters */
         // There are none...
         
@@ -75,13 +129,14 @@ class TMDBClient : NSObject {
                 return
             }
             
+            returnData = data
             /* 5/6. Parse the data and use the data (happens in completion handler) */
             completionHandlerForImage(data, nil)
         }
         
         /* 7. Start the request */
         task.resume()
-        
+        return returnData!
     }
     
     // MARK: Helpers
@@ -96,18 +151,18 @@ class TMDBClient : NSObject {
     }
     
     // given raw JSON, return a usable Foundation object
-//    private func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
-//        
-//        var parsedResult: AnyObject! = nil
-//        do {
-//            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
-//        } catch {
-//            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
-//            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
-//        }
-//        
-//        completionHandlerForConvertData(parsedResult, nil)
-//    }
+    //    private func convertDataWithCompletionHandler(_ data: Data, completionHandlerForConvertData: (_ result: AnyObject?, _ error: NSError?) -> Void) {
+    //
+    //        var parsedResult: AnyObject! = nil
+    //        do {
+    //            parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+    //        } catch {
+    //            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+    //            completionHandlerForConvertData(nil, NSError(domain: "convertDataWithCompletionHandler", code: 1, userInfo: userInfo))
+    //        }
+    //
+    //        completionHandlerForConvertData(parsedResult, nil)
+    //    }
     
     // create a URL from parameters
     class func tmdbURLFromParameters(_ parameters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
